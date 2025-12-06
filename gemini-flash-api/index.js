cc75e5d508786c9201fa95110c092f0547055809
afrizal <muhammadafrizal239@gmail.com>
2025-12-06 21:05:36 +0700

    update index.js

--- gemini-flash-api/index.js
+++ gemini-flash-api/index.js
@@ -4,14 +4,14 @@ import multer from 'multer';
 import fs from 'fs/promises' ;
 import { GoogleGenAI } from "@google/genai";
 
-const app = express ( ) ;
-const upload = multer ( ) ;
-const ai = new GoogleGenAI( { apiKey: process. env.API_KEY }) ;
+const app = express();
+const upload = multer();
+const ai = new GoogleGenAI( { apiKey: process.env.API_KEY }) ;
 
 // ** Set your default Gemini model here :**
 const GEMINI_MODEL = "gemini-2.5-flash";
 
-app. use(express. json());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server ready on http://localhost:${PORT}`));
@@ -21,13 +21,34 @@ app.post('/generate-text', async (req, res) => {
     const { prompt } = req.body;
 
     try {
-        const model = ai.getGenerativeModel(GEMINI_MODEL);
-        const result = await model.generateContent(prompt);
-        const response = result.response;
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: prompt
+        });
 
-        res.status(200).json({ result: response.text() });
+        res.status(200).json({ result: response.text });
     } catch (e) {
         console.log(e) ;
+        res.status(500).json({ message: e.message });
+    }
+});
+
+app.post("/generate-from-image", upload.single("image"), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Image = req.file.buffer.toString("base64");
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt, type: "text" },
+                { inlineData: { data: base64Image, mimeType: req.file.mimetype } }
+            ],
+        });
+
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
         res.status(500).json({ message: e.message });
     }
 });
